419590a3d65fd200738df6653cf2718ac494f52b
jolenic <jennifer.olenic@gmail.com>
2020-09-21 20:55:41 -0400

    Everything is now working in Grid Layout

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         final Button button1 = findViewById(R.id.button1);
         final String toast_message = getResources().getString(R.string.toast_message);
